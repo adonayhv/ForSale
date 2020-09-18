7290632bb1ba03f0036ff73dff6bae82c540775a
adonayhv <adonayvh@gmail.com>
2020-09-18 09:53:00 -0600

    MultiLenguaje

---
+++ ForSale.XamarinApp/ForSale.XamarinApp/Helpers/Languages.cs
@@ -0,0 +1,30 @@
+﻿using ForSale.ComunDll.Helpers;
+using ForSale.XamarinApp.Resources;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ForSale.XamarinApp.Helpers
+{
+    public static class Languages
+    {
+        static Languages()
+        {
+            CultureInfo ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+            Resource.Culture = ci;
+            Culture = ci.Name;
+            DependencyService.Get<ILocalize>().SetLocale(ci);
+        }
+
+        public static string Culture { get; set; }
+
+        public static string Accept => Resource.Accept;
+
+        public static string ConnectionError => Resource.ConnectionError;
+
+        public static string Error => Resource.Error;
+    }
+
+}
